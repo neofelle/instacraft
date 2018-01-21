25c521674461e0a3bec3cdfade263e17c71475fa
bisqy <neofelle@gmail.com>
2018-01-21 18:53:45 +0800

    mobile fix

---
+++ application/logs/log-2018-01-21.php
@@ -0,0 +1,185 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2018-01-21 11:30:03 --> Config Class Initialized
+INFO - 2018-01-21 11:30:03 --> Hooks Class Initialized
+DEBUG - 2018-01-21 11:30:03 --> UTF-8 Support Enabled
+INFO - 2018-01-21 11:30:03 --> Utf8 Class Initialized
+INFO - 2018-01-21 11:30:03 --> URI Class Initialized
+INFO - 2018-01-21 11:30:03 --> Router Class Initialized
+INFO - 2018-01-21 11:30:03 --> Output Class Initialized
+INFO - 2018-01-21 11:30:03 --> Security Class Initialized
+DEBUG - 2018-01-21 11:30:03 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-01-21 11:30:03 --> Input Class Initialized
+INFO - 2018-01-21 11:30:03 --> Language Class Initialized
+INFO - 2018-01-21 11:30:03 --> Language Class Initialized
+INFO - 2018-01-21 11:30:03 --> Config Class Initialized
+INFO - 2018-01-21 11:30:03 --> Loader Class Initialized
+INFO - 2018-01-21 11:30:03 --> Helper loaded: url_helper
+INFO - 2018-01-21 11:30:03 --> Helper loaded: file_helper
+INFO - 2018-01-21 11:30:03 --> Helper loaded: user_helper
+INFO - 2018-01-21 11:30:04 --> Helper loaded: json_helper
+INFO - 2018-01-21 11:30:04 --> Helper loaded: admin_helper
+INFO - 2018-01-21 11:30:04 --> Helper loaded: driver_helper
+INFO - 2018-01-21 11:30:04 --> Helper loaded: cookie_helper
+INFO - 2018-01-21 11:30:04 --> Helper loaded: common_helper
+INFO - 2018-01-21 11:30:04 --> Language file loaded: language/english/en_lang.php
+INFO - 2018-01-21 11:30:04 --> Database Driver Class Initialized
+ERROR - 2018-01-21 11:30:04 --> Unable to connect to the database
+DEBUG - 2018-01-21 11:30:04 --> Email Class Initialized
+DEBUG - 2018-01-21 11:30:04 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2018-01-21 11:30:04 --> Session: Class initialized using 'files' driver.
+INFO - 2018-01-21 11:30:04 --> Helper loaded: form_helper
+INFO - 2018-01-21 11:30:04 --> Form Validation Class Initialized
+INFO - 2018-01-21 11:30:04 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2018-01-21 11:30:04 --> Pagination Class Initialized
+INFO - 2018-01-21 11:30:04 --> User Agent Class Initialized
+INFO - 2018-01-21 11:30:04 --> Controller Class Initialized
+DEBUG - 2018-01-21 11:30:04 --> Doctor MX_Controller Initialized
+INFO - 2018-01-21 11:30:04 --> Model Class Initialized
+DEBUG - 2018-01-21 11:30:04 --> File loaded: C:\xampp\htdocs\insta17\application\modules/website/models/Doctor_model.php
+INFO - 2018-01-21 11:30:04 --> Model Class Initialized
+DEBUG - 2018-01-21 11:30:04 --> File loaded: C:\xampp\htdocs\insta17\application\modules/website/models/Login_model.php
+INFO - 2018-01-21 11:30:04 --> Model Class Initialized
+DEBUG - 2018-01-21 11:30:04 --> S3 Class Initialized
+INFO - 2018-01-21 11:30:04 --> Model Class Initialized
+ERROR - 2018-01-21 11:30:04 --> Severity: error --> Exception: Call to a member function real_escape_string() on boolean C:\xampp\htdocs\insta17\system\database\drivers\mysqli\mysqli_driver.php 392
+INFO - 2018-01-21 11:30:23 --> Config Class Initialized
+INFO - 2018-01-21 11:30:23 --> Hooks Class Initialized
+DEBUG - 2018-01-21 11:30:23 --> UTF-8 Support Enabled
+INFO - 2018-01-21 11:30:23 --> Utf8 Class Initialized
+INFO - 2018-01-21 11:30:23 --> URI Class Initialized
+INFO - 2018-01-21 11:30:23 --> Router Class Initialized
+INFO - 2018-01-21 11:30:23 --> Output Class Initialized
+INFO - 2018-01-21 11:30:23 --> Security Class Initialized
+DEBUG - 2018-01-21 11:30:23 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-01-21 11:30:23 --> Input Class Initialized
+INFO - 2018-01-21 11:30:23 --> Language Class Initialized
+INFO - 2018-01-21 11:30:23 --> Language Class Initialized
+INFO - 2018-01-21 11:30:23 --> Config Class Initialized
+INFO - 2018-01-21 11:30:23 --> Loader Class Initialized
+INFO - 2018-01-21 11:30:23 --> Helper loaded: url_helper
+INFO - 2018-01-21 11:30:23 --> Helper loaded: file_helper
+INFO - 2018-01-21 11:30:23 --> Helper loaded: user_helper
+INFO - 2018-01-21 11:30:23 --> Helper loaded: json_helper
+INFO - 2018-01-21 11:30:23 --> Helper loaded: admin_helper
+INFO - 2018-01-21 11:30:23 --> Helper loaded: driver_helper
+INFO - 2018-01-21 11:30:23 --> Helper loaded: cookie_helper
+INFO - 2018-01-21 11:30:23 --> Helper loaded: common_helper
+INFO - 2018-01-21 11:30:23 --> Language file loaded: language/english/en_lang.php
+INFO - 2018-01-21 11:30:23 --> Database Driver Class Initialized
+ERROR - 2018-01-21 11:30:23 --> Unable to connect to the database
+DEBUG - 2018-01-21 11:30:23 --> Email Class Initialized
+DEBUG - 2018-01-21 11:30:23 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2018-01-21 11:30:23 --> Session: Class initialized using 'files' driver.
+INFO - 2018-01-21 11:30:23 --> Helper loaded: form_helper
+INFO - 2018-01-21 11:30:23 --> Form Validation Class Initialized
+INFO - 2018-01-21 11:30:23 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2018-01-21 11:30:23 --> Pagination Class Initialized
+INFO - 2018-01-21 11:30:23 --> User Agent Class Initialized
+INFO - 2018-01-21 11:30:23 --> Controller Class Initialized
+DEBUG - 2018-01-21 11:30:23 --> Doctor MX_Controller Initialized
+INFO - 2018-01-21 11:30:33 --> Config Class Initialized
+INFO - 2018-01-21 11:30:33 --> Hooks Class Initialized
+DEBUG - 2018-01-21 11:30:33 --> UTF-8 Support Enabled
+INFO - 2018-01-21 11:30:33 --> Utf8 Class Initialized
+INFO - 2018-01-21 11:30:33 --> URI Class Initialized
+INFO - 2018-01-21 11:30:33 --> Router Class Initialized
+INFO - 2018-01-21 11:30:33 --> Output Class Initialized
+INFO - 2018-01-21 11:30:33 --> Security Class Initialized
+DEBUG - 2018-01-21 11:30:33 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-01-21 11:30:33 --> Input Class Initialized
+INFO - 2018-01-21 11:30:33 --> Language Class Initialized
+INFO - 2018-01-21 11:30:33 --> Language Class Initialized
+INFO - 2018-01-21 11:30:33 --> Config Class Initialized
+INFO - 2018-01-21 11:30:33 --> Loader Class Initialized
+INFO - 2018-01-21 11:30:33 --> Helper loaded: url_helper
+INFO - 2018-01-21 11:30:33 --> Helper loaded: file_helper
+INFO - 2018-01-21 11:30:33 --> Helper loaded: user_helper
+INFO - 2018-01-21 11:30:33 --> Helper loaded: json_helper
+INFO - 2018-01-21 11:30:33 --> Helper loaded: admin_helper
+INFO - 2018-01-21 11:30:33 --> Helper loaded: driver_helper
+INFO - 2018-01-21 11:30:33 --> Helper loaded: cookie_helper
+INFO - 2018-01-21 11:30:33 --> Helper loaded: common_helper
+INFO - 2018-01-21 11:30:33 --> Language file loaded: language/english/en_lang.php
+INFO - 2018-01-21 11:30:33 --> Database Driver Class Initialized
+ERROR - 2018-01-21 11:30:33 --> Unable to connect to the database
+DEBUG - 2018-01-21 11:30:33 --> Email Class Initialized
+DEBUG - 2018-01-21 11:30:33 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2018-01-21 11:30:33 --> Session: Class initialized using 'files' driver.
+INFO - 2018-01-21 11:30:33 --> Helper loaded: form_helper
+INFO - 2018-01-21 11:30:33 --> Form Validation Class Initialized
+INFO - 2018-01-21 11:30:33 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2018-01-21 11:30:33 --> Pagination Class Initialized
+INFO - 2018-01-21 11:30:33 --> User Agent Class Initialized
+INFO - 2018-01-21 11:30:33 --> Controller Class Initialized
+DEBUG - 2018-01-21 11:30:33 --> Doctor MX_Controller Initialized
+INFO - 2018-01-21 11:30:43 --> Config Class Initialized
+INFO - 2018-01-21 11:30:43 --> Hooks Class Initialized
+DEBUG - 2018-01-21 11:30:43 --> UTF-8 Support Enabled
+INFO - 2018-01-21 11:30:43 --> Utf8 Class Initialized
+INFO - 2018-01-21 11:30:43 --> URI Class Initialized
+INFO - 2018-01-21 11:30:43 --> Router Class Initialized
+INFO - 2018-01-21 11:30:43 --> Output Class Initialized
+INFO - 2018-01-21 11:30:43 --> Security Class Initialized
+DEBUG - 2018-01-21 11:30:43 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-01-21 11:30:43 --> Input Class Initialized
+INFO - 2018-01-21 11:30:43 --> Language Class Initialized
+INFO - 2018-01-21 11:30:43 --> Language Class Initialized
+INFO - 2018-01-21 11:30:43 --> Config Class Initialized
+INFO - 2018-01-21 11:30:43 --> Loader Class Initialized
+INFO - 2018-01-21 11:30:43 --> Helper loaded: url_helper
+INFO - 2018-01-21 11:30:43 --> Helper loaded: file_helper
+INFO - 2018-01-21 11:30:43 --> Helper loaded: user_helper
+INFO - 2018-01-21 11:30:43 --> Helper loaded: json_helper
+INFO - 2018-01-21 11:30:43 --> Helper loaded: admin_helper
+INFO - 2018-01-21 11:30:43 --> Helper loaded: driver_helper
+INFO - 2018-01-21 11:30:43 --> Helper loaded: cookie_helper
+INFO - 2018-01-21 11:30:43 --> Helper loaded: common_helper
+INFO - 2018-01-21 11:30:43 --> Language file loaded: language/english/en_lang.php
+INFO - 2018-01-21 11:30:43 --> Database Driver Class Initialized
+ERROR - 2018-01-21 11:30:43 --> Unable to connect to the database
+DEBUG - 2018-01-21 11:30:43 --> Email Class Initialized
+DEBUG - 2018-01-21 11:30:43 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2018-01-21 11:30:43 --> Session: Class initialized using 'files' driver.
+INFO - 2018-01-21 11:30:43 --> Helper loaded: form_helper
+INFO - 2018-01-21 11:30:43 --> Form Validation Class Initialized
+INFO - 2018-01-21 11:30:43 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2018-01-21 11:30:43 --> Pagination Class Initialized
+INFO - 2018-01-21 11:30:43 --> User Agent Class Initialized
+INFO - 2018-01-21 11:30:43 --> Controller Class Initialized
+DEBUG - 2018-01-21 11:30:43 --> Doctor MX_Controller Initialized
+INFO - 2018-01-21 11:30:53 --> Config Class Initialized
+INFO - 2018-01-21 11:30:53 --> Hooks Class Initialized
+DEBUG - 2018-01-21 11:30:53 --> UTF-8 Support Enabled
+INFO - 2018-01-21 11:30:53 --> Utf8 Class Initialized
+INFO - 2018-01-21 11:30:53 --> URI Class Initialized
+INFO - 2018-01-21 11:30:53 --> Router Class Initialized
+INFO - 2018-01-21 11:30:53 --> Output Class Initialized
+INFO - 2018-01-21 11:30:53 --> Security Class Initialized
+DEBUG - 2018-01-21 11:30:53 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-01-21 11:30:53 --> Input Class Initialized
+INFO - 2018-01-21 11:30:53 --> Language Class Initialized
+INFO - 2018-01-21 11:30:53 --> Language Class Initialized
+INFO - 2018-01-21 11:30:53 --> Config Class Initialized
+INFO - 2018-01-21 11:30:53 --> Loader Class Initialized
+INFO - 2018-01-21 11:30:53 --> Helper loaded: url_helper
+INFO - 2018-01-21 11:30:53 --> Helper loaded: file_helper
+INFO - 2018-01-21 11:30:53 --> Helper loaded: user_helper
+INFO - 2018-01-21 11:30:53 --> Helper loaded: json_helper
+INFO - 2018-01-21 11:30:53 --> Helper loaded: admin_helper
+INFO - 2018-01-21 11:30:53 --> Helper loaded: driver_helper
+INFO - 2018-01-21 11:30:53 --> Helper loaded: cookie_helper
+INFO - 2018-01-21 11:30:53 --> Helper loaded: common_helper
+INFO - 2018-01-21 11:30:53 --> Language file loaded: language/english/en_lang.php
+INFO - 2018-01-21 11:30:53 --> Database Driver Class Initialized
+ERROR - 2018-01-21 11:30:53 --> Unable to connect to the database
+DEBUG - 2018-01-21 11:30:53 --> Email Class Initialized
+DEBUG - 2018-01-21 11:30:53 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2018-01-21 11:30:53 --> Session: Class initialized using 'files' driver.
+INFO - 2018-01-21 11:30:53 --> Helper loaded: form_helper
+INFO - 2018-01-21 11:30:53 --> Form Validation Class Initialized
+INFO - 2018-01-21 11:30:53 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2018-01-21 11:30:53 --> Pagination Class Initialized
+INFO - 2018-01-21 11:30:53 --> User Agent Class Initialized
+INFO - 2018-01-21 11:30:53 --> Controller Class Initialized
+DEBUG - 2018-01-21 11:30:53 --> Doctor MX_Controller Initialized
