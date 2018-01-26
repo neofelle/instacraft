3cdf36e4c4ae7551eaf1d4e2f44f29068b05c606
Bryann Revina <bryann@theshiftleft.com>
2018-01-26 15:45:24 -0800

    Updated enscroll speed

--- application/modules/customer/views/mobile/footer.php
+++ application/modules/customer/views/mobile/footer.php
@@ -865,7 +865,7 @@
     $(function () {
         var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
         if (!isMobile) {
-            $('.container').enscroll();
+            $('.container').enscroll({scrollIncrement:140});
         }
 
     })
