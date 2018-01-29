0ce36ae875ff90cce5cfc656912ac81220607779
Bryann Revina <bryann@theshiftleft.com>
2018-01-29 14:24:53 -0800

    updated git ignore

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$url = "";
-$config['base_url'] = ""; // http://".$_SERVER["HTTP_HOST"]."/$url/
+$url = "instacraft";
+$config['base_url'] = "http://localhost/farris/git/instacraft/"; 
 $config['customer']	= 'customer';
 $config['customerassets']	= $config['base_url'].'assets/'.$config['customer'].'/';
 
