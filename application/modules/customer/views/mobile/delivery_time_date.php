21d14ed0275898f4b4288075e0bc4052cade0fa9
Bryann Revina <bryann@theshiftleft.com>
2018-01-30 19:58:54 -0800

    updated checkout map

--- application/modules/customer/views/mobile/delivery_time_date.php
+++ application/modules/customer/views/mobile/delivery_time_date.php
@@ -5,8 +5,9 @@
         <div class="map_container">
             <div class="map_box" id="map_delivery">
             </div>
-            <div class="delivery_form">
-                <input class="txt-field" type="text" name="address" id="delivery_address" placeholder="Current Address">
+            <div class="delivery_form" style="bottom:1px;">
+                <input class="txt-field" type="text" name="address" id="start_address" placeholder="Current Address">
+                <input class="txt-field" type="text" name="address" id="delivery_address" placeholder="Destination Address">
                 <input class="txt-field" type="text" name="date_time" id="delivery_date_time" placeholder="Select Delivery Date & Time">
                 <input class="txt-field" type="hidden" name="delivery_lat_lng" id="delivery_lat_lng">
             </div>
@@ -168,6 +169,74 @@
     });
     
     
+    //Custom
+    $(function(){
+        getLocation();
+    });    
+    var x=document.getElementById("start_address");
+    function getLocation(){        
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(showPosition,showError);
+        }
+        else{
+            x.innerHTML="Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position){
+        lat=position.coords.latitude;
+        lon=position.coords.longitude;
+        displayLocation(lat,lon);
+    }
+
+    function showError(error){
+        switch(error.code){
+            case error.PERMISSION_DENIED:
+                x.innerHTML="User denied the request for Geolocation."
+            break;
+            case error.POSITION_UNAVAILABLE:
+                x.innerHTML="Location information is unavailable."
+            break;
+            case error.TIMEOUT:
+                x.innerHTML="The request to get user location timed out."
+            break;
+            case error.UNKNOWN_ERROR:
+                x.innerHTML="An unknown error occurred."
+            break;
+        }
+    }
+
+    function displayLocation(latitude,longitude){        
+        var geocoder;
+        geocoder = new google.maps.Geocoder();
+        var latlng = new google.maps.LatLng(latitude, longitude);
+
+        geocoder.geocode(
+            {'latLng': latlng}, 
+            function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    if (results[0]) {
+                        var add= results[0].formatted_address ;
+                        var  value=add.split(",");
+
+                        count=value.length;
+                        country=value[count-1];
+                        state=value[count-2];
+                        city=value[count-3];                        
+                        $("#start_address").val(city);
+                        x.innerHTML = city;
+                    }
+                    else  {
+                        $("#start_address").val('');                        
+                    }
+                }
+                else {
+                    $("#start_address").val('');                                            
+                }
+            }
+        );
+    }
+    //
     
     function initMap() {
        
@@ -181,10 +250,15 @@
     });
     var geocoder = new google.maps.Geocoder();
     var input = document.getElementById('delivery_address');
+    var input_from = document.getElementById('start_address');
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input_from);
 
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
+
+    var autocompleteB = new google.maps.places.Autocomplete(input_from);
+    //autocompleteB.bindTo('bounds', map);
 
     var infowindow = new google.maps.InfoWindow();
     
